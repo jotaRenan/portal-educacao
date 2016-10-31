26b9fd0080182fa8241eed001e96e7d283e4386d
João Pedro Renan <jota.renan@gmail.com>
2016-10-31 10:36:28 -0200

    Modifica login (WIP)

---
+++ Ger%C3%AAncia/LoginPHP/Login.php
@@ -0,0 +1,14 @@
+<?php
+	include 'ClasseLogin.php';
+	$nome = $_GET['nome'];
+	$senha = $GET['senha'];
+	$tipo = $GET['tipo'];
+
+ 	if ( $usuario = Login::logarUsuario($nome, $senha, $tabela, $tipo)) {
+ 		session_start();
+ 		$_SESSION['usuario'] = $usuario;	
+ 		
+ 	}
+
+
+?>
